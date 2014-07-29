8e5aac9cbbe10280809100b8ac1cb6cd92afa92c
Daniel Wise <danielwise@live.com>
2014-07-28 20:39:20 -0700

    apply cname fix from Cap2 to Cap3. Removing downcase command

--- lib/elbow/capistrano.rb
+++ lib/elbow/capistrano.rb
@@ -9,7 +9,7 @@ def elastic_load_balancer(name, *args)
     packet = Net::DNS::Resolver.start(name)
     all_cnames= packet.answer.reject { |p| !p.instance_of? Net::DNS::RR::CNAME }
     cname = all_cnames.find { |c| c.name == "#{name}."}
-    cname = cname.cname[0..-2]
+    elb_name = cname ? cname.cname[0..-2] : name
 
     aws_region= fetch(:aws_region, 'us-east-1')
     AWS.config(:access_key_id => fetch(:aws_access_key_id),
@@ -17,7 +17,7 @@ def elastic_load_balancer(name, *args)
         :ec2_endpoint => "ec2.#{aws_region}.amazonaws.com",
         :elb_endpoint => "elasticloadbalancing.#{aws_region}.amazonaws.com")
 
-    load_balancer = AWS::ELB.new.load_balancers.find { |elb| elb.dns_name.downcase == cname.downcase }
+    load_balancer = AWS::ELB.new.load_balancers.find { |elb| elb.dns_name == elb_name }
     raise "EC2 Load Balancer not found for #{name} in region #{aws_region}" if load_balancer.nil?
 
     load_balancer.instances.each do |instance|
